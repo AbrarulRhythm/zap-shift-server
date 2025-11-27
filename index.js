14bf4694cb50785b7bcfcd9c00599f2f895e65a9
Abrarul Rhythm <[....email>
2025-11-28 02:05:07 +0600

    added stripr payment method

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const app = express();
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const stripe = require('stripe')(process.env.STRIPE_SECRET);
+
 const port = process.env.PORT || 3000;
 
 // Middleeare
@@ -52,6 +54,14 @@ async function run() {
             res.send(result);
         });
 
+        // Get API for single data
+        app.get('/parcels/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await parcelsCollections.findOne(query);
+            res.send(result);
+        });
+
         // Post API
         app.post('/parcels', async (req, res) => {
             const parcel = req.body;
@@ -66,6 +76,36 @@ async function run() {
 
             const result = await parcelsCollections.deleteOne(query);
             res.send(result);
+        });
+
+        // :::::::::::::::::::::::::::::: - Payment Related APIS - ::::::::::::::::::::::::::::::
+        app.post('/create-checkout-session', async (req, res) => {
+            const paymentInfo = req.body;
+            const amount = parseInt(paymentInfo.cost) * 100;
+
+            const session = await stripe.checkout.sessions.create({
+                line_items: [
+                    {
+                        price_data: {
+                            currency: 'USD',
+                            unit_amount: amount,
+                            product_data: {
+                                name: paymentInfo.parcleName
+                            }
+                        },
+                        quantity: 1,
+                    },
+                ],
+                customer_email: paymentInfo.senderEmail,
+                mode: 'payment',
+                metadata: {
+                    parcelId: paymentInfo.parcelId
+                },
+                success_url: `${process.env.SITE_DOMAIN}/dashboard/payment-success`,
+                cancel_url: `${process.env.SITE_DOMAIN}/dashboard/payment-cancelled`,
+            });
+
+            res.send({ url: session.url });
         });
 
         // Send a ping to confirm a successful connection
