94304ffe7fc551f43b2ed22cae2d42e56c7d3646
Abrarul Rhythm <[....email>
2025-11-29 00:06:35 +0600

    change tracking id formate

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function generateTrackingId() {
     const date = new Date().toISOString().slice(0, 10).replace(/-/g, ''); // YYYYMMDD
     const random = crypto.randomBytes(3).toString('hex').toUpperCase(); // 6 char random hex
 
-    return `${prefix}-${date}-${random}`;
+    return `${prefix}_${date}_${random}`;
 }
 
 // Middleeare
